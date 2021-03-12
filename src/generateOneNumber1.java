0338a084c6c2c494b674a19998039046135b8e6b
guisoacelga <guiso.acelga@gmail.com>
2021-03-12 17:05:16 +0100

    refactor and add generateOneNumber1.java

--- src/generateOneNumber1.java
+++ src/generateOneNumber1.java
@@ -14,13 +14,13 @@ public class generateOneNumber1 {
         System.out.println("----------- ********* ----------- ");
         System.out.println("Now, I am going to generate five random positive numbers in a range (1 to " + range + ") and print them below.");
         System.out.println("But I had previously saved them in an array and then print from the array!");
-        generateFiveRandomNumbersArray(range);
+        generateFiveRandomNumbersArrayPrint(range);
 
         System.out.println("----------- ********* ----------- ");
         System.out.println("Now, I am going to generate five random positive numbers in a range (1 to " + range + ") and print them below.");
         System.out.println("But I had previously saved them in an array and then print from the array!");
         System.out.println("Extra level of difficulty: print them in ascending order :O");
-
+        sortArrayAscending(generateFiveRandomNumbersArrayReturn(range));
 
     }
 
@@ -48,7 +48,7 @@ public class generateOneNumber1 {
         }
     }
 
-    public static void generateFiveRandomNumbersArray(int range){
+    public static void generateFiveRandomNumbersArrayPrint(int range){
         int[] arrayRandom = new int[5];
         System.out.print("And the numbers are : ");
 
@@ -57,10 +57,57 @@ public class generateOneNumber1 {
         }
 
         for (int j = 0; j < arrayRandom.length; j++) {
-            if (j == 4) {
+            if (j == arrayRandom.length -1) {
                 System.out.print(arrayRandom[j] + ". \n");
             } else {
                 System.out.print(arrayRandom[j] + ", ");
+            }
+        }
+
+    }
+
+    public static int[] generateFiveRandomNumbersArrayReturn(int range){
+        int[] arrayRandomReturn = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            arrayRandomReturn[i] = generateOneRandomNumber(range);
+        }
+
+        return arrayRandomReturn;
+    }
+
+    public static void sortArrayAscending(int[] arrayToSort) {
+        System.out.print("First let's print the original array: ");
+        for (int j = 0; j < arrayToSort.length; j++) {
+            if (j == arrayToSort.length -1) {
+                System.out.print(arrayToSort[j] + ". \n");
+            } else {
+                System.out.print(arrayToSort[j] + ", ");
+            }
+        }
+
+
+        for (int i = 0; i < arrayToSort.length; i++)            //for anidado necesitamos dos contadores i y j
+        {
+            for (int j = i + 1; j < arrayToSort.length; j++)    //es importante setear j a i+1 xq empieza a chequear en el segundo
+            {
+                int tmp = 0;
+                if (arrayToSort[i] > arrayToSort[j])
+                {
+                    tmp = arrayToSort[i];
+                    arrayToSort[i] = arrayToSort[j];
+                    arrayToSort[j] = tmp;
+                }
+            }
+        }
+
+        //TODO: ver que no se repitan los números
+        System.out.print("And now the magic happens!! Here is the sorted array: ");
+        for (int j = 0; j < arrayToSort.length; j++) {
+            if (j == arrayToSort.length -1) {                   //imprimir hasta uno antes del largo del array
+                System.out.print(arrayToSort[j] + ". \n");
+            } else {
+                System.out.print(arrayToSort[j] + ", ");
             }
         }
 
